ca5d78b91aa2292b16ceea3b33af5a796c576fd6
German Attanasio Ruiz <germanattanasio@gmail.com>
2015-08-10 18:22:31 -0400

    jshint and code formatting

--- WebContent/Domain.js
+++ WebContent/Domain.js
@@ -1,42 +1,43 @@
 /**
  * Copyright 2015 IBM Corp. All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
  */
+'use strict';
+/*global define, dojo */
 
-define([ "dojo/_base/declare" ], function(declare) {
-  return declare("retail.Domain", [], {
+define(['dojo/_base/declare'], function(declare) {
+  return declare('retail.Domain', [], {
 
-    constructor : function(meta, options) {
+    constructor: function(meta, options) {
       this.meta = meta;
       this.filters = {};
       this.allOps = options;
       this.opsIn = options;
 
       dojo.forEach(this.allOps, function(op) {
-        op.changed = function() {
-        };
+        op.changed = function() {};
       });
       this.analyzeFrontier(this.opsIn);
     },
 
     // var selectedSortOp = meta.sortBy.filter(function(op){return
     // op.selected;})[0];
-    addFilter : function(name, filter, quite) {
+    addFilter: function(name, filter, quite) {
       this.filters[name].push(filter);
       this.applyFilters(quite);
     },
-    applyFilters : function(quite) {
+    applyFilters: function(quite) {
       var oldOpsIn = this.opsIn.slice();
       this._doFilters();
       if (!(quite || oldOpsIn.equals(this.opsIn))) {
@@ -45,19 +46,19 @@ define([ "dojo/_base/declare" ], function(declare) {
         this.changed();
       }
     },
-    _doFilters : function() {
+    _doFilters: function() {
       this.opsIn = dojo.filter(this.allOps, function(op) {
-        for ( var facet in this.filters) {
+        for (var facet in this.filters) {
           if (!this.filters[facet].some(function(facetValFilter) {
-            return facetValFilter(op);
-          })) {
+              return facetValFilter(op);
+            })) {
             return false;
           }
         }
         return true;
       }, this);
     },
-    setSortBy : function(op) {
+    setSortBy: function(op) {
       var oldOpsIn = this.opsIn.slice();
       this.sortBy = op;
       this.sort();
@@ -66,32 +67,34 @@ define([ "dojo/_base/declare" ], function(declare) {
       }
     },
     // sortBy : selectedSortOp,
-    sort : function() {
+    sort: function() {
       if (this.sortBy) {
         var sortAttKey = this.sortBy.attribute;
         var sortAtt = this.meta.attributes.first(function(a) {
-          return a.key == sortAttKey;
+          return a.key === sortAttKey;
         });
         var comparer = undefined;
-        if (sortAtt.type == "NUMERIC") {
+        if (sortAtt.type === 'NUMERIC') {
           comparer = function(a, b) {
-            var va = a[sortAttKey], vb = b[sortAttKey];
+            var va = a[sortAttKey],
+              vb = b[sortAttKey];
             return va - vb;
-          }
+          };
         } else {
           comparer = function(a, b) {
-            var va = a[sortAttKey], vb = b[sortAttKey];
+            var va = a[sortAttKey],
+              vb = b[sortAttKey];
             return va < vb ? -1 : vb < va ? 1 : 0;
-          }
+          };
         }
         this.opsIn.sort(comparer);
-        if (this.sortBy.direction == "Descending") {
+        if (this.sortBy.direction === 'Descending') {
           this.opsIn.reverse();
         }
       }
     },
     // enrich the data and meta objects with frontier information
-    analyzeFrontier : function(options, callback) {
+    analyzeFrontier: function(options, callback) {
       if (!options) {
         return;
       }
@@ -103,61 +106,61 @@ define([ "dojo/_base/declare" ], function(declare) {
         callback && callback();
       } else {
         var meta = this.meta;
-        var dilemmaUrl = "demo?generate_visualization=false";
+        var dilemmaUrl = 'demo?generate_visualization=false';
+
         function afterDilemma(response) {
           var respObj = JSON.parse(response);
           options.forEach(function(op) {
             var ref = respObj.resolution.solutions.first(function(ref) {
-              return meta.keyFunction(op) == ref.solution_ref
+              return meta.keyFunction(op) === ref.solution_ref;
             });
-            op.favorite = (ref.status == "FRONT");
+            op.favorite = (ref.status === 'FRONT');
             op.changed();
           });
           callback && callback();
         }
 
-        xhr(dilemmaUrl, "POST", {}, JSON.stringify(this.makeProblem(options)), afterDilemma, function(e) {
+        xhr(dilemmaUrl, 'POST', {}, JSON.stringify(this.makeProblem(options)), afterDilemma, function(e) {
           throw e;
         });
       }
     },
-    makeProblem : function(options) {
+    makeProblem: function(options) {
       this.meta.attributes.forEach(function(col) {
         if (!col.name) {
           col.fullName = col.key;
         }
       });
       var numAtts = this.meta.attributes.filter(function(att) {
-        return att.type === "NUMERIC";
+        return att.type === 'NUMERIC';
       });
       var objCols = dojo.map(numAtts, function(att) {
         return {
-          is_objective : att.isObjective || (this.sortBy && (this.sortBy.attribute == att.key)),
-          goal : att.goal,
-          key : att.key,
-          full_name : att.name || att.key,
-          type : att.type
+          is_objective: att.isObjective || (this.sortBy && (this.sortBy.attribute === att.key)),
+          goal: att.goal,
+          key: att.key,
+          full_name: att.name || att.key,
+          type: att.type
         };
       }, this);
       return {
-        subject : this.meta.subject,
-        columns : objCols,
-        options : dojo.map(options, function(op) {
+        subject: this.meta.subject,
+        columns: objCols,
+        options: dojo.map(options, function(op) {
           var values = {};
           numAtts.forEach(function(col) {
             values[col.key] = parseFloat(op[col.key]);
           });
           return {
-            key : this.meta.keyFunction(op),
-            name : this.meta.nameFunction(op),
-            description_html : this.meta.descriptionFunction(op),
+            key: this.meta.keyFunction(op),
+            name: this.meta.nameFunction(op),
+            description_html: this.meta.descriptionFunction(op),
             // app_data : op,
-            values : values
-          }
+            values: values
+          };
         }, this)
       };
     },
-    changed : function() {/* event */
-    }
-  })
+    changed: function() { /* event */ }
+  });
 });
